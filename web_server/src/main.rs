e1ce82e233201d305da67bcfdf3d928aed70980b
MikkoP <penttila.mikko@gmail.com>
2023-10-22 21:05:46 +0300

    Korjattu typo web_serverissä

--- web_server/src/main.rs
+++ web_server/src/main.rs
@@ -2,14 +2,12 @@ use warp::Filter;
 
 #[tokio::main]
 async fn main() {
-    println!("Listening to port 8085")
+    println!("Listening to port 8085");
     let index = warp::path::end()
         .and(warp::fs::file("static/index.html"))
-        .boxed();  // Box the filter to ensure it resolves to a Reply
+        .boxed(); // Box the filter to ensure it resolves to a Reply
 
     let routes = index;
 
-    warp::serve(routes)
-        .run(([0, 0, 0, 0], 8085))
-        .await;
+    warp::serve(routes).run(([0, 0, 0, 0], 8085)).await;
 }
