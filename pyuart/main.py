75a608b9125b2ec327c3c87ac81183943008755a
Mikko Penttilä <penttila.mikko@gmail.com>
2023-10-27 23:23:34 +0300

    Lisätty pyuart projekti repoon

---
+++ pyuart/main.py
@@ -0,0 +1,13 @@
+import serial
+import urllib.request
+import json
+import time
+
+s = serial.Serial("/dev/serial0", 9600, timeout=1)  # TODO: Check that the serial dev and baud are actually correct
+s.flush()
+
+while True:
+    res = json.loads(urllib.request.urlopen("http://127.0.0.1:8081/temperature").read().decode("utf-8"))
+    res = res["t"]["temperature"]  # NOTE! These are tenths of celsius!
+    s.write(str(res).encode("utf-8"))
+    time.sleep(10)
