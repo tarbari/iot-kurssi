4901621c55e9bfd4f61c73d175d0bd2993577a58
Mikko Penttilä <penttila.mikko@gmail.com>
2023-10-28 16:07:42 +0300

    Refaktoroitu pyuartiin edes pientä virheenkäsittelyä

--- pyuart/main.py
+++ pyuart/main.py
@@ -3,11 +3,33 @@ import urllib.request
 import json
 import time
 
-s = serial.Serial("/dev/serial0", 115200, timeout=1)  # TODO: Check that the serial dev and baud are actually correct
-s.flush()
 
-while True:
-    res = json.loads(urllib.request.urlopen("http://127.0.0.1:8081/temperature").read().decode("utf-8"))
-    res = res["t"]["temperature"]  # NOTE! These are tenths of celsius!
-    s.write(str(res).encode("utf-8"))
-    time.sleep(10)
+SERIAL_PORT = "/dev/serial0"
+BAUD_RATE = 115200
+API_ENDPOINT = "http://127.0.0.1:8081/temperature"
+POLL_INTERVAL = 10
+
+
+def fetch_temperature():
+    try:
+        res = urllib.request.urlopen(API_ENDPOINT)
+        data = json.loads(res.read().decode("utf-8"))
+        temperature = data["t"]["temperature"] / 10  # Converting to full celsius from tenths
+        return temperature
+    except Exception:
+        # Could use some error logging...
+        return None
+
+
+def main():
+    with serial.Serial(SERIAL_PORT, BAUD_RATE, timeout=1) as s:
+        while True:
+            temperature = fetch_temperature()
+            if temperature is not None:
+                s.write(str(temperature).encode("utf-8"))
+            time.sleep(POLL_INTERVAL)
+
+
+if __name__ == "__main__":
+    main()
+
